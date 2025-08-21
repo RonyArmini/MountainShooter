c0a44201cc4556519c7a9d5ea301471e84b6a72f
RonyArmini <ronyarmani@gmail.com>
2025-08-21 00:09:18 -0300

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -9,6 +9,7 @@ C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 
 ENTITY_SHOT_DELAY = {
     'Player1': 20,
@@ -25,6 +26,11 @@ ENTITY_DAMAGE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
@@ -44,6 +50,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -63,6 +74,11 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -82,6 +98,11 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 1,
+    'Level2Bg1': 2,
+    'Level2Bg2': 3,
+    'Level2Bg3': 4,
+    'Level2Bg4': 5,
     'Player1': 3,
     'Player1Shot': 1,
     'Player2': 3,
@@ -114,6 +135,10 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_SPACE,
 
 # S
 SPAWN_TIME = 4000
+
+#
+TIMEOUT_STEP = 100 # 100ms
+TIMEOUT_LEVEL = 20000 # 20 segundos
 
 # W
 WIN_WIDTH = 576
